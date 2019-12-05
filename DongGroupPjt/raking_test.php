1647c51acb2cd52b9c7210ac487c7cd8b1fa7137
belbo <maplelyy@naver.com>
2019-12-05 18:21:27 +0900

    데이터  추가

--- DongGroupPjt/raking_test.php
+++ DongGroupPjt/raking_test.php
@@ -10,6 +10,7 @@
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
 		<link rel="stylesheet" href="assets/css/main.css" />
+
 	</head>
 	<body class="is-preload">
 
@@ -23,8 +24,15 @@
 							<!-- Header -->
 								<header id="header">
 									<a href="index.html" class="logo"><strong>Editorial</strong> by HTML5 UP</a>
-									
+									<ul class="icons">
+										<li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
+										<li><a href="#" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
+										<li><a href="#" class="icon brands fa-snapchat-ghost"><span class="label">Snapchat</span></a></li>
+										<li><a href="#" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
+										<li><a href="#" class="icon brands fa-medium-m"><span class="label">Medium</span></a></li>
+									</ul>
 								</header>
+
 							<!-- Content -->
 								<section>
 									<header class="main">
@@ -157,7 +165,6 @@
 										</tbody>
 									</table>
 									<input type="submit" value="저장하기">
-									</form>
 									<?php
 									// error_reporting(E_ALL);
 
@@ -282,6 +289,8 @@
 									?>
 									
 
+
+
 								</section>
 
 						</div>
@@ -289,79 +298,79 @@
 
 				<!-- Sidebar -->
 				<div id="sidebar">
-						<div class="inner">
+					<div class="inner">
 
-							<!-- Search -->
-								<section id="search" class="alt">
-									<form method="post" action="#">
-										<input type="text" name="query" id="query" placeholder="Search" />
-									</form>
-								</section>
+						<!-- Search -->
+							<section id="search" class="alt">
+								<form method="post" action="#">
+									<input type="text" name="query" id="query" placeholder="Search" />
+								</form>
+							</section>
 
-							<!-- Menu -->
-								<nav id="menu">
-									<header class="major">
-										<h2>Menu</h2>
-									</header>
-									<ul>
-										<li><a href="index.html">Homepage</a></li>
-										<li><a href="register.php">REGISTER</a></li>
-										<li><a href="raking.php">ranking</a></li>
-										<li><a href="sample.php">DB Connection</a></li>
-									</ul>소
-								</nav>
+						<!-- Menu -->
+							<nav id="menu">
+								<header class="major">
+									<h2>Menu</h2>
+								</header>
+								<ul>
+									<li><a href="index.html">Homepage</a></li>
+									<li><a href="register.php">REGISTER</a></li>
+									<li><a href="raking.php">ranking</a></li>
+									<li><a href="sample.php">DB Connection</a></li>
+								</ul>
+							</nav>
 
-							<!-- Section -->
-								<section>
-									<header class="major">
-										<h2>Ante interdum</h2>
-									</header>
-									<div class="mini-posts">
-										<article>
-											<a href="#" class="image"><img src="images/pic07.jpg" alt="" /></a>
-											<p>회의실 1사진이 들어갈 자리입니다.</p>
-										</article>
-										<article>
-											<a href="#" class="image"><img src="images/pic08.jpg" alt="" /></a>
-											<p>회의 2 사진이 들어갈 자리! </p>
-										</article>
-										<article>
-											<a href="#" class="image"><img src="images/pic09.jpg" alt="" /></a>
-											<p>대충 사진찍어서 하나더 넣습니다!</p>
-										</article>
-									</div>
-									<ul class="actions">
-										<li><a href="#" class="button">간지용버튼</a></li>
-									</ul>
-								</section>
+						<!-- Section -->
+							<section>
+								<header class="major">
+									<h2>Ante interdum</h2>
+								</header>
+								<div class="mini-posts">
+									<article>
+										<a href="#" class="image"><img src="images/pic07.jpg" alt="" /></a>
+										<p>회의실 1사진이 들어갈 자리입니다.</p>
+									</article>
+									<article>
+										<a href="#" class="image"><img src="images/pic08.jpg" alt="" /></a>
+										<p>회의 2 사진이 들어갈 자리! </p>
+									</article>
+									<article>
+										<a href="#" class="image"><img src="images/pic09.jpg" alt="" /></a>
+										<p>대충 사진찍어서 하나더 넣습니다!</p>
+									</article>
+								</div>
+								<ul class="actions">
+									<li><a href="#" class="button">간지용버튼</a></li>
+								</ul>
+							</section>
 
-							<!-- Section -->
-								<section>
-									<header class="major">
-										<h2>Get in touch</h2>
-									</header>
-									<p>저희 조는 GitHub를 이용해서 협업 하였습니다. 소스를 공유 해드립니다. 그외 문의나 서버 구축에 대해서 문의가 있으면 아래 정보로 연락을 주세요 </p>
-									<ul class="contact">
-										<li class="icon solid fa-envelope"><a href="#">maplelyy@naver.com</a></li>
-										<li class="icon solid fa-phone">mapelyy (카톡)</li>
-										<li class="icon solid fa-home"><a href="https://github.com/Belbos/DonggukPjt">https://github.com/Belbos/<br/>DonggukPjt.git</a></li>
-									</ul>
-								</section>
+						<!-- Section -->
+							<section>
+								<header class="major">
+									<h2>Get in touch</h2>
+								</header>
+								<p>저희 조는 GitHub를 이용해서 협업 하였습니다. 소스를 공유 해드립니다. 그외 문의나 서버 구축에 대해서 문의가 있으면 아래 정보로 연락을 주세요 </p>
+								<ul class="contact">
+									<li class="icon solid fa-envelope"><a href="#">maplelyy@naver.com</a></li>
+									<li class="icon solid fa-phone">mapelyy (카톡)</li>
+									<li class="icon solid fa-home"><a href="https://github.com/Belbos/DonggukPjt">https://github.com/Belbos/<br/>DonggukPjt.git</a></li>
+								</ul>
+							</section>
 
-							<!-- Footer -->
-								<footer id="footer">
-									<p class="copyright">&copy; Untitled. All rights reserved. Demo Images: <a href="https://unsplash.com">Unsplash</a>. Design: <a href="https://html5up.net">HTML5 UP</a>.</p>
-								</footer>
+						<!-- Footer -->
+							<footer id="footer">
+								<p class="copyright">&copy; Untitled. All rights reserved. Demo Images: <a href="https://unsplash.com">Unsplash</a>. Design: <a href="https://html5up.net">HTML5 UP</a>.</p>
+							</footer>
 
-						</div>
 					</div>
+				</div>
 
-		<!-- Scripts -->
-			<script src="assets/js/jquery.min.js"></script>
+			</div>
+
+
 			<script src="assets/js/browser.min.js"></script>
 			<script src="assets/js/breakpoints.min.js"></script>
 			<script src="assets/js/util.js"></script>
 			<script src="assets/js/main.js"></script>
-
 	</body>
 </html>
